cfc4a1f9c3993cde1e86eb524bfb355c714b5b86
Victor <vhsouza606@gmail.com>
2021-04-27 15:35:41 -0300

    reafactor: remake the user model

--- server/src/domain/models/User.ts
+++ server/src/domain/models/User.ts
@@ -1,6 +1,46 @@
-export type User = {
+import { InvalidParamError, MissingParamError } from '../errors'
+
+interface IUser {
   id: string
   name: string
   email: string
   password: string
+  validate: (params: Omit<IUser, 'validate'>) => void
+}
+
+export class User implements IUser {
+  id: string
+  email: string
+  password: string
+  name: string
+
+  constructor(params: IUser) {
+    this.validate(params)
+
+    this.id = params.id
+    this.email = params.email
+    this.password = params.password
+    this.name = params.name
+  }
+
+  validate(params: Omit<IUser, 'validate'>) {
+    Object.entries(params).forEach(([param, value]) => {
+      if (!value.trim()) {
+        throw new MissingParamError(param)
+      }
+    })
+
+    if (params.name.length <= 2 || params.name.length >= 256) {
+      throw new InvalidParamError('name')
+    }
+
+    if (params.password.length <= 2 || params.password.length >= 256) {
+      throw new InvalidParamError('password')
+    }
+    const emailTester = /^[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/
+
+    if (!emailTester.test(params.email)) {
+      throw new InvalidParamError('email')
+    }
+  }
 }
