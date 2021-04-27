263a2583e07e7f89e5b46cff6d9945dcb25217d4
Victor <vhsouza606@gmail.com>
2021-04-27 16:11:16 -0300

    feat: create user use case factory

---
+++ server/src/main/factories/CreateUserFactory.ts
@@ -0,0 +1,18 @@
+import { BcryptAdapter } from '../../data/crypto'
+import { UserRepository } from '../../data/repositories'
+import { UuidAdapter } from '../../data/utils'
+import { CreateUser } from '../../domain/useCases'
+
+export const createCreateUser = () => {
+  const userRepository = new UserRepository()
+  const uuidAdapter = new UuidAdapter()
+  const bcryptAdapter = new BcryptAdapter()
+
+  const createUserUseCase = new CreateUser(
+    userRepository,
+    uuidAdapter,
+    bcryptAdapter
+  )
+
+  return createUserUseCase
+}
