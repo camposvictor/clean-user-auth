e86fd09f539d50a29c2a6c39fd54370983f0b87c
Victor <vhsouza606@gmail.com>
2021-05-01 19:35:42 -0300

    feat: add login validator to factory

--- server/src/main/factories/LoginControllerFactory.ts
+++ server/src/main/factories/LoginControllerFactory.ts
@@ -1,8 +1,11 @@
 import { LoginController } from '../../presentation/controllers/LoginController'
+import { YupLoginValidator } from '../../presentation/validation'
 import { makeAuthentication } from './AuthenticationFactory'
 
 export const makeLoginController = () => {
-  const loginController = new LoginController(makeAuthentication())
+  const validator = new YupLoginValidator()
+  const authenticationUseCase = makeAuthentication()
+  const loginController = new LoginController(authenticationUseCase, validator)
 
   return loginController
 }
