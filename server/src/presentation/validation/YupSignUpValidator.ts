f91ef6f94a21ca6fe3e3fb3941d2b77084b3e8a0
Victor <vhsouza606@gmail.com>
2021-05-01 19:18:14 -0300

    refactor: sign up validator

--- server/src/presentation/validation/YupSignUpValidator.ts
+++ server/src/presentation/validation/YupSignUpValidator.ts
@@ -1,26 +1,14 @@
-import { IValidator } from '../protocols'
 import * as yup from 'yup'
+import { YupValidator } from '.'
 
-type Params = {
-  email: string
-  name: string
-  password: string
-}
-
-export class YupSignUpValidator implements IValidator {
-  schema: any
+export class YupSignUpValidator extends YupValidator {
   constructor() {
-    this.schema = yup.object().shape({
+    const schema = yup.object().shape({
       email: yup.string().email().required().max(256),
       name: yup.string().required().min(2).max(256),
       password: yup.string().required().min(6).max(256),
     })
-  }
-  async validate(params: Params) {
-    try {
-      await this.schema.validate(params)
-    } catch (error: yup.ValidationError | any) {
-      return error.errors
-    }
+
+    super(schema)
   }
 }
