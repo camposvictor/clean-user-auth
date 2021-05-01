58d9a1d0f4dc6a752943a9f40ca72a428b9a2c5c
Victor <vhsouza606@gmail.com>
2021-05-01 20:03:54 -0300

    refactor: sign up controller returns null body on success

--- server/src/presentation/controllers/SignUpController.ts
+++ server/src/presentation/controllers/SignUpController.ts
@@ -29,7 +29,7 @@ export class SignUpController implements IController {
 
       return {
         statusCode: 201,
-        body: {},
+        body: null,
       }
     } catch (error) {
       return {
