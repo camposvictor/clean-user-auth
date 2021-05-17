d107437bbaa77f8960d4d85524bf9a64ad2b7e42
Victor <vhsouza606@gmail.com>
2021-05-17 09:38:22 -0300

    refactor: put express app in separeted file

--- server/src/main/server.ts
+++ server/src/main/server.ts
@@ -1,12 +1,5 @@
-import express from 'express'
-import router from './routes'
+import { app } from './app'
 
-const app = express()
-app.use(express.json())
-app.use(router)
-
-const server = app.listen(3333, () => {
+app.listen(3333, () => {
   console.log('Server running')
 })
-
-export { server }
