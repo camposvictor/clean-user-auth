93f799bdb67e9233d53f438423259daa2c961e72
Victor <vhsouza606@gmail.com>
2021-07-03 16:35:35 -0300

    feat: create http helper functions

---
+++ server/src/presentation/helpers/httpHelper.ts
@@ -0,0 +1,50 @@
+import { HttpResponse } from '../protocols'
+
+export function badRequest(message: string): HttpResponse {
+  return {
+    statusCode: 400,
+    body: {
+      message,
+    },
+  }
+}
+
+export function notFound(message: string): HttpResponse {
+  return {
+    statusCode: 404,
+    body: {
+      message,
+    },
+  }
+}
+
+export function conflict(message: string): HttpResponse {
+  return {
+    statusCode: 401,
+    body: {
+      message,
+    },
+  }
+}
+
+export function ok(dto: any): HttpResponse {
+  return {
+    statusCode: 200,
+    body: dto,
+  }
+}
+
+export function created(dto: any): HttpResponse {
+  return {
+    statusCode: 201,
+    body: dto,
+  }
+}
+export function serverError(message: string): HttpResponse {
+  return {
+    statusCode: 500,
+    body: {
+      message,
+    },
+  }
+}
